3719950d53cfb785e1f8e5e605fffd9a66bd53fb
Khushali Vadadoriya <vkhushali18@gmail.com>
2024-02-21 00:24:37 +0530

    css fix

--- script.js
+++ script.js
@@ -21,6 +21,20 @@ hoverElements.forEach(function (element) {
   });
 });
 
+var MenuCursor = document.querySelectorAll(".nav-menu-list a");
+MenuCursor.forEach(function (elem) {
+  elem.addEventListener("mouseenter", function () {
+    Cursor.style.scale = 3;
+    Cursor.style.backgroundColor = "transparent";
+    Cursor.style.border = "1px solid #fff";
+  });
+  elem.addEventListener("mouseleave", function () {
+    Cursor.style.scale = 1;
+    Cursor.style.border = "0px solid #95C11E";
+    Cursor.style.backgroundColor = "#95C11E";
+  });
+});
+
 gsap.to("#nav", {
   backgroundColor: "#000",
   padding: "1rem 5.5%",
@@ -39,9 +53,69 @@ gsap.to("#main", {
   scrollTrigger: {
     trigger: "#main",
     scroller: "body",
-    markers: true,
     start: "top -25%",
     end: "top -70%",
     scrub: 2,
+  },
+});
+
+gsap.from(".about-us-wrapper img, .about-us-txt", {
+  y: 50,
+  opacity: 0,
+  duration: 1,
+  scrollTrigger: {
+    trigger: ".about-us",
+    scroller: "body",
+    start: "top 70%",
+    end: "top 65%",
+    scrub: 3,
+  },
+});
+
+gsap.from(".card", {
+  scale: 0.8,
+  opacity: 0,
+  scrollTrigger: {
+    trigger: ".card-inner",
+    scroller: "body",
+    start: "top 70%",
+    end: "top 65%",
+    scrub: 3,
+  },
+});
+
+gsap.from("#colon1", {
+  y: -70,
+  x: -70,
+  scrollTrigger: {
+    trigger: "#colon1",
+    scroller: "body",
+    // markers:true,
+    start: "top 55%",
+    end: "top 45%",
+    scrub: 4,
+  },
+});
+gsap.from("#colon2", {
+  y: 70,
+  x: 70,
+  scrollTrigger: {
+    trigger: "#colon1",
+    scroller: "body",
+    // markers:true,
+    start: "top 55%",
+    end: "top 45%",
+    scrub: 4,
+  },
+});
+gsap.from("#page4 h1", {
+  y: 50,
+  scrollTrigger: {
+    trigger: "#page4",
+    scroller: "body",
+    // markers:true,
+    start: "top 75%",
+    end: "top 70%",
+    scrub: 3,
   },
 });
